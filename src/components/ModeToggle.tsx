e4edc2305da8a360e4dac88b6fe590127ab31875
LukeHenningSmith <lukeusydwork@gmail.com>
2025-03-01 10:40:26 +1100

    Add darkmode polygon animation

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -6,17 +6,21 @@ import { useTheme } from "@/components/theme-provider";
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
 
+  const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    document.documentElement.classList.remove(theme);
+    document.documentElement.classList.add(newTheme);
+    setTheme(newTheme);
+  };
+
   return (
     <Button
-      variant="outline"
+      variant={"ghost"}
       size="icon"
       onClick={() => {
-        if (theme === "light") {
-          setTheme("dark");
-        } else {
-          setTheme("light");
-        }
+        document.startViewTransition(switchTheme);
       }}
+      className="cursor-pointer"
     >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:rotate-90 dark:scale-0" />
