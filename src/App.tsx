7dcb4e908e29b4d135b310428fa439657c55d34d
LukeHenningSmith <lukeusydwork@gmail.com>
2025-03-03 21:37:47 +1100

    Wrap app in TooltipProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,29 @@ import { Route, Routes } from "react-router-dom";
 import { routesConfig } from "./routes";
 import { Breadcrumbs } from "./components/Breadcrumbs";
 import Footer from "./components/footer/Footer";
+import { TooltipProvider } from "./components/ui/tooltip";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark">
-      <Header config={routesConfig} />
-      <div className="App" style={{ width: "800px" }}>
-        <Breadcrumbs />
-        <div className="w-full flex-col justify-center items-center px-8">
-          <Routes>
-            {routesConfig.map((route) => (
-              <Route
-                key={route.id}
-                path={route.path}
-                element={<route.component />}
-              />
-            ))}
-          </Routes>
+      <TooltipProvider>
+        <Header config={routesConfig} />
+        <div className="App" style={{ width: "800px" }}>
+          <Breadcrumbs />
+          <div className="w-full flex-col justify-center items-center px-8">
+            <Routes>
+              {routesConfig.map((route) => (
+                <Route
+                  key={route.id}
+                  path={route.path}
+                  element={<route.component />}
+                />
+              ))}
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
+      </TooltipProvider>
     </ThemeProvider>
   );
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="flex flex-col justify-center items-center m-8 gap-8">
       <Separator />
-      <p className="text-zinc-500">Luke Henning-Smith 2024</p>
+      <p className="text-zinc-500">Luke Henning-Smith 2025</p>
     </div>
   );
 }
